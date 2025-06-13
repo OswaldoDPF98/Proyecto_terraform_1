e2afa5d12627f012f1c62e9b83ca1039f3903f70
OswaldoDPF98 <oswaldodpf98@gmail.com>
2025-06-13 08:20:51 -0300

    Creando el repositorio

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  profile = "mi_perfil"
+  region  = "us-east-1"
+}
+
+
+
+# 1. Crear una VPC
+# 2. Crear un Internet Gateway
+# 3. Crear una Subnet
+# 4. Crear una Route Table
+# 5. Asociar la Route Table a la Subnet
+# 6. Crear un grupo de seguridad para los puertos 22, 443 y 80
+# 7. Crear un Network Interface con una IP en la Subnet creada en el paso 3
+# 8. Asignar una IP elástica al Network Interface creado en el paso 7
+# 9. Crear un servidor ubuntu e instalar/habilitar apache2
